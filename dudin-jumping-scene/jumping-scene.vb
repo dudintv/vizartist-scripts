4ca14979141ec9fb5ac747d69bb8ccc8748ba120
ddudin@TVC <svetoz@gmail.com>
2018-12-26 13:14:08 +0300

    translate to English

--- dudin-jumping-scene/jumping-scene.vb
+++ dudin-jumping-scene/jumping-scene.vb
@@ -1,8 +1,8 @@
-Dim info As String = "Скрипт для загрузки прыгающих сцен.
-Смотрит что загружено в среднем и верхнем слое.
-При необходимости создает копию сцены и загружает её в верхний слой.
-При первой загрузке сразу делается копия.
-Разработчик: Дудин Дмитрий. Версия 0.1 (3 октября 2018)
+Dim info As String = "Script for smart loading 'jumping' scenes.
+It looks what loaded in front and middle layers.
+Create scene duplicate and load in front layer if it needed.
+Make two duplicates at first loading.
+Developer: Dmitry Dudin. Version 0.2 (26 dec 2018)
 "
 
 Dim loadedSceneInMiddle, loadedSceneInFront, pathForLoad, sceneUUID, sceneInfo, uuid, sceneRef, sceneID As String
@@ -11,47 +11,37 @@ Dim arr_scenes As Array[String]
 Dim needCheckCountSceneInPool As Boolean
 
 Sub Load(scenePath As String)
-	println("")
-	println("")
-	println(3,"scenePath = " & scenePath)
     loadedSceneInMiddle = System.SendCommand("RENDERER*LOCATION_PATH GET")
 	loadedSceneInFront  = ""
 	if CInt(System.FrontScene) <> 0 then
 		loadedSceneInFront  = System.SendCommand("RENDERER*FRONT_LAYER*LOCATION_PATH GET")
 	end if
-	println("loadedSceneInMiddle = " & loadedSceneInMiddle)
-	println("loadedSceneInFront  = " & loadedSceneInFront)
 	sceneUUID = System.SendCommand("RENDERER*UUID GET")
-	println("sceneUUID = " & sceneUUID)
 	sceneInfo = System.SendCommand("SCENE INFO")
-	println("sceneInfo = " & sceneInfo)
 	sceneInfo.Split("\n", arr_scenes)
 	needCheckCountSceneInPool = false
+	
 	if loadedSceneInFront == "SCENE*" & scenePath then
-		'если сцена уже есть во FRONT слое — не успела спрынуть — перехватываем её, все равно cut-склейка неминуема
-		println(2,"CATCH SCENE FROM FRONT LAYER")
+		'if the front layer have the scene (the scene did not have time to jump) we just catch it and use. We can't avoid cut of frames.
         pathForLoad = "SCENE*" & System.SendCommand("RENDERER*FRONT_LAYER*OBJECT_ID GET")
     elseif loadedSceneInMiddle <> "SCENE*" & scenePath then
-		'если в среднем слое другая сцена то берем сцену просто как обычно
+		'if the middle layer have another scene we get the scene as usual
         pathForLoad = "SCENE*" & ScenePath
 		needCheckCountSceneInPool = true
 	else
-		'итак, ситуация ради которой все затевалось:
-		'в среднем слое такая же сцена и надо 
-		'найти или создать копию этой сцены для независимой выдачи
+		'there is the same scene in middle layer
+		'we have to find or create copy of the scene for independent using
 	    zeroRefCount = 0
 	
 	    for each scene in arr_scenes
 	        scene.Trim()
 	        if scene <> "" then
 				uuid = GetUUID(scene)
-				println("current uuid = " & uuid)
 	
 	            if uuid == sceneUUID then
 					countScenesInPool += 1
 					ref = scene.Find("#ref=")
 	                sceneRef = scene.GetSubstring(ref+5,1)
-					println("current sceneRef = " & sceneRef)
 	
 	                if sceneRef == "0" then
 	                    pathForLoad = "SCENE*" & scene.Left(scene.Find(","))
@@ -67,37 +57,29 @@ Sub Load(scenePath As String)
 	        end if
 	    next
 		
-		println("zeroRefCount = " & zeroRefCount)
 	    if zeroRefCount <= 0 then
-			'дубликат уже нужен! создаем и используем
+			'we sure duplicate must be created
 			pathForLoad = System.SendCommand(sceneUUID & " DUPLICATE")
 	    end If
     end If
 
-    
-	println("pathForLoad = " & pathForLoad)
 	System.SendCommand("RENDERER*FRONT_LAYER SET_OBJECT " & pathForLoad)
 	
 	if needCheckCountSceneInPool then
-		'предусмотрительно смотрим - эта сцена уже имеет копии?
+		'Looking providently if the scene already have copies...
 		sceneUUID = System.SendCommand("RENDERER*FRONT_LAYER*UUID GET")
 		countScenesInPool = 0
 		for each scene in arr_scenes
 	        scene.Trim()
-			println("| cur scene = " & scene)
 	        if scene <> "" AND GetUUID(scene) == sceneUUID then
 				countScenesInPool += 1
 	        end if
 	    next
-		println("countScenesInPool = " & countScenesInPool)
 		if countScenesInPool <= 1 then
-			'предусмотрительно делаем копию на будущее
+			'Making additional copy of scene for the future
 			System.SendCommand(sceneUUID & " DUPLICATE")
 	    end If
 	end if
-	
-	println("")
-	println("")
 End Sub
 
 Function GetUUID(s As String) As String
