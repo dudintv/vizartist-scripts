bc1588542abd034a48df4478ce588e6b66ca1e32
ddudin@TVC <svetoz@gmail.com>
2019-01-10 19:30:21 +0300

    Translate Autoscroll to English

--- dudin-autoscroll/autoscroll.vb
+++ dudin-autoscroll/autoscroll.vb
@@ -1,13 +1,14 @@
 Dim info As String = "
-Скприт подстраивается под внешние габариты и должен регулировать:
-1. ширину блока типа как MaxSize, но без тормозов
-2. анимацию скроллинга по всей своей поверхности в пределах фона
-Версия 0.5 (18 апреля 2018)
+The script adjust width to outer width like MaxSize plugin with min and max scale.
+And gives ability to animate like scrolling within gabarit container.
+Version 0.6 (10 January 2019)
 "
 
+'SETTINGS
+Dim treshold As Double = 0.01
+
+'STUFF
 Dim fon As Container
-'Dim screen1, screen2 As Vertex
-'Dim old_screen1, old_screen2 As Vertex
 Dim v1, v2, old_v1, old_v2, v_local1, v_local2 As Vertex
 Dim v_me1, v_me2, old_v_me1, old_v_me2 As Vertex
 Dim paddingX, paddingY, old_paddingX, old_paddingY, new_x, new_y, new_width, new_height As Double
@@ -20,36 +21,31 @@ Dim geom As Geometry
 Dim liveMode, inverseAnim As Boolean
 Dim lessMode, moreMode As Integer
 Dim keyStart, keyEnd As Keyframe
-
-'for inertion
+'for inertion:
 Dim target_y, inertion As Double
-Dim treshold As Double = 0.01
 
 Dim arrMode As Array[String]
-arrMode.Push("Наверху")
-arrMode.Push("По центру")
-arrMode.Push("Внизу")
-arrMode.Push("По проценту")
+arrMode.Push("Top")
+arrMode.Push("Center")
+arrMode.Push("Bottom")
+arrMode.Push("By percent")
 
 sub OnInitParameters()
 	RegisterInfoText(info)
-	RegisterParameterContainer("fon", "Фоновый контейнер:")
-	RegisterParameterDouble("paddingX", "Смещение границ X", 0.0, -1000.0, 1000.0)
-	RegisterParameterDouble("paddingY", "Смещение границ Y", 0.0, -1000.0, 1000.0)
-	RegisterParameterDouble("min_scale", "Мин. scale", 0, 0, 999999.0)
-	RegisterParameterDouble("max_scale", "Макс. scale", 999999.0, 0, 999999.0)
-	RegisterParameterBool("live_mode", "Постоянный режим", true)
-	RegisterPushButton("set_up", "Показать верх", 1)
-	RegisterPushButton("set_down", "Показать низ", 2)
-	RegisterPushButton("show_percent", "Показать на процент", 3)
-	RegisterParameterDouble("scroll", "Процент скролла", 0.0, -100.0, 200.0)
-	RegisterParameterBool("inverse_anim", "Инвертировать направление анимации?", true)
-	RegisterRadioButton("less_mode", "Когда высота меньше", 1, arrMode)
-	RegisterRadioButton("more_mode", "Когда высота больше", 1, arrMode)
-	RegisterParameterDouble("time_start_scroll", "Начало скролла (сек)", 0, 0, 100000.0)
-	RegisterParameterDouble("time_period_scroll", "Длительность скролла (сек)", 5.0, 0, 100000.0)
-	RegisterPushButton("create_keys", "Сделать ключи анимации", 4)
-	RegisterParameterDouble("inertion", "Инерция", 0.0, 0, 1000.0)
+	RegisterParameterContainer("fon", "Gabarit container:")
+	RegisterParameterDouble("paddingX", "X padding", 0.0, -1000.0, 1000.0)
+	RegisterParameterDouble("paddingY", "Y padding", 0.0, -1000.0, 1000.0)
+	RegisterParameterDouble("min_scale", "Min scale", 0, 0, 999999.0)
+	RegisterParameterDouble("max_scale", "Max scale", 999999.0, 0, 999999.0)
+	RegisterParameterDouble("scroll", "Scroll percent", 0.0, -100.0, 200.0)
+	
+	RegisterRadioButton("less_mode", "If height less then gabarit", 1, arrMode)
+	RegisterRadioButton("more_mode", "If height greater then gabarit", 1, arrMode)
+	RegisterParameterDouble("time_start_scroll", "Scroll begin (sec)", 0, 0, 100000.0)
+	RegisterParameterDouble("time_period_scroll", "Scroll duration (sec)", 5.0, 0, 100000.0)
+	RegisterPushButton("create_keys", "Create animation keyframes", 1)
+	RegisterParameterBool("inverse_anim", "Invert keyframe position", true)
+	RegisterParameterDouble("inertion", "Inertion", 0.0, 0, 1000.0)
 end sub
 
 sub OnInit()
@@ -59,6 +55,7 @@ sub OnInit()
 	lessMode = GetParameterInt("less_mode")
 	moreMode = GetParameterInt("more_mode")
 end sub
+
 sub OnParameterChanged(parameterName As String)
 	OnInit()
 	PrepareAndPosX()
@@ -68,25 +65,13 @@ sub OnParameterChanged(parameterName As String)
 	inverseAnim = GetParameterBool("inverse_anim")
 end sub
 
-
 '-----------------------------------------------------------------
-
 
 sub OnExecAction(buttonId As Integer)
 	if buttonId == 1 then
-		PrepareAndPosX()
-		SetByPercent(0)
-	elseif buttonId == 2 then
-		PrepareAndPosX()
-		SetByPercent(1)
-	elseif buttonId == 3 then
-		PrepareAndPosX()
-		SetByPercent(scroll)
-	elseif buttonId == 4 then
 		CreateKeys()
 	end if
 end sub
-
 
 Sub PrepareAndPosX()
 	fon.RecomputeMatrix()
@@ -103,8 +88,6 @@ Sub PrepareAndPosX()
 	new_height = v_local2.y - v_local1.y
 	
 	If new_width > 0.001 AND new_height > 0.001 Then
-		'установка ширины
-		'pMax.SetParameterDouble("WIDTH_X",new_width + paddingX)
 		targetSize = v_local2.x - v_local1.x
 		meSize = v_me2.x - v_me1.x
 		
@@ -116,7 +99,6 @@ Sub PrepareAndPosX()
 		if newScaleY < minScale then newScaleY = minScale
 		this.scaling.x = newScaleY
 		this.scaling.y = newScaleY
-		'установка позиции по X
 		this.Position.X = v_local1.x - v_me1.x*this.scaling.x - this.scaling.x*paddingX*10.0/2.0
 	End If
 End Sub
@@ -131,11 +113,9 @@ Sub SetByPercent(scrl as double)
 	target_y = (v_local2.y+scrl*(v_local1.y-v_local2.y)) - (v_me2.y+scrl*(v_me1.Y-v_me2.Y))*this.Scaling.Y - paddingY*(scrl-0.5)
 	
 	if IsNearToTarget() then
-		'set to target
 		this.Position.Y = target_y
 		target_y = this.Position.Y
 	else
-		'inertion animation
 		this.Position.y += (target_y - this.Position.y)/(1.0 + GetParameterDouble("inertion"))
 	end if
 End Sub
@@ -173,7 +153,6 @@ Sub CreateKeys()
 	timeStart = GetParameterDouble("time_start_scroll")
 	timeEnd = timeStart + GetParameterDouble("time_period_scroll")
 
-
 	chScroll = this.FindOrCreateChannelOfObject("scroll")
 	if chScroll <> null then chScroll.Delete()
 	chScroll = this.ScriptPluginInstance.FindOrCreateChannelOfObject("scroll")
@@ -190,7 +169,7 @@ Sub CreateKeys()
 End Sub
 
 sub OnExecPerField()
-	If fon == null OR liveMode == false Then Exit Sub
+	If fon == null Then Exit Sub
 	
 	fon.GetTransformedBoundingBox(v1, v2)
 	this.GetBoundingBox(v_me1, v_me2)
