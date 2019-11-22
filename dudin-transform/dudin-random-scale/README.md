48a5c49a44a0d8d33877eb17330c2fdeabd006d3
Dmitry Dudin <svetoz@gmail.com>
2019-11-22 11:55:09 +0100

    add Random Transform by Scale

---
+++ dudin-transform/dudin-random-scale/README.md
@@ -0,0 +1,4 @@
+## Randomly fluctuations by scaling
+
+#### Version 1.0 (22 november 2019)
+* common scaling with random period
---
+++ dudin-transform/dudin-random-scale/dudin-random-scale.vb
@@ -0,0 +1,110 @@
+RegisterPluginVersion(1,0,0)
+Dim amplitude, scale_speed, speed, value, base_scale As Double
+Dim max, max_speed As Double
+Dim dir As Integer
+
+sub OnInitParameters()
+	RegisterParameterDouble("base_scale", "Base scale", 1.0, 0, 999999)
+	RegisterParameterDouble("amplitude", "Scale amplitude %", 20.0, 0, 100)
+	RegisterParameterDouble("scale_speed", "Speed", 0, 0, 999999)
+end sub
+
+sub OnInit()
+	scale_speed = GetParameterDouble("scale_speed")
+	base_scale = GetParameterDouble("base_scale")
+	amplitude = base_scale * GetParameterDouble("amplitude")/100.0
+	value = 0
+	speed = (scale_speed/2.0 + (scale_speed/2.0)*Random())/100.0
+	dir = 1
+end sub
+sub OnParameterChanged(parameterName As String)
+	OnInit()
+end sub
+
+sub OnExecPerField()
+	if value >= 100 then dir = -1
+	if value <= 0 then dir = 1
+	value += dir*speed
+	
+	this.scaling.xyz = base_scale + 2*amplitude * (Besizer(value, 0, 1, 50, 50)-0.5)
+end sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'CLAMP
+Function ClampDbl(value as double, min  as double, max as double) as Double
+	if value < min then value = min
+	if value > max then value = max
+	ClampDbl = value
+End Function
+
+'BESIER
+Function Besizer(ByVal procent as double, ByVal begin_value as double, ByVal end_value as double, ByVal begin_weight as double, ByVal end_weight as double) as Double
+	Dim a, b, c, d, t_besier_value As Double
+	procent      = ClampDbl(procent,       0, 100)/100.0
+	begin_weight = ClampDbl(begin_weight, 35, 100)/100.0
+	end_weight   = ClampDbl(end_weight,   35, 100)/100.0
+	
+	a = 3*begin_weight - 3*(1.0 - end_weight) + 1
+	b = - 6*begin_weight + 3*(1.0 - end_weight)
+	c = 3*begin_weight
+	d = -procent
+	
+	t_besier_value = (sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)/(3*2^(1.0/3)*a) - (2^(1.0/3)*(3*a*c - b^2))/(3*a*(sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)) - b/(3*a)
+	Besizer = begin_value + (end_value - begin_value)*( 3*(1-t_besier_value)*t_besier_value^2 + t_besier_value^3 ) 
+End Function
+
+Dim amplitude, scale_speed, speed, value, base_scale As Double
+Dim max, max_speed As Double
+Dim dir As Integer
+
+sub OnInitParameters()
+	RegisterParameterDouble("base_scale", "Base scale", 1.0, 0, 999999)
+	RegisterParameterDouble("amplitude", "Scale amplitude %", 20.0, 0, 100)
+	RegisterParameterDouble("scale_speed", "Speed", 0, 0, 999999)
+end sub
+
+sub OnInit()
+	scale_speed = GetParameterDouble("scale_speed")
+	base_scale = GetParameterDouble("base_scale")
+	amplitude = base_scale * GetParameterDouble("amplitude")/100.0
+	value = 0
+	speed = (scale_speed/2.0 + (scale_speed/2.0)*Random())/100.0
+	dir = 1
+end sub
+sub OnParameterChanged(parameterName As String)
+	OnInit()
+end sub
+
+sub OnExecPerField()
+	if value >= 100 then dir = -1
+	if value <= 0 then dir = 1
+	value += dir*speed
+	
+	this.scaling.xyz = base_scale + 2*amplitude * (Besizer(value, 0, 1, 50, 50)-0.5)
+end sub
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+'CLAMP
+Function ClampDbl(value as double, min  as double, max as double) as Double
+	if value < min then value = min
+	if value > max then value = max
+	ClampDbl = value
+End Function
+
+'BESIER
+Function Besizer(ByVal procent as double, ByVal begin_value as double, ByVal end_value as double, ByVal begin_weight as double, ByVal end_weight as double) as Double
+	Dim a, b, c, d, t_besier_value As Double
+	procent      = ClampDbl(procent,       0, 100)/100.0
+	begin_weight = ClampDbl(begin_weight, 35, 100)/100.0
+	end_weight   = ClampDbl(end_weight,   35, 100)/100.0
+	
+	a = 3*begin_weight - 3*(1.0 - end_weight) + 1
+	b = - 6*begin_weight + 3*(1.0 - end_weight)
+	c = 3*begin_weight
+	d = -procent
+	
+	t_besier_value = (sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)/(3*2^(1.0/3)*a) - (2^(1.0/3)*(3*a*c - b^2))/(3*a*(sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)) - b/(3*a)
+	Besizer = begin_value + (end_value - begin_value)*( 3*(1-t_besier_value)*t_besier_value^2 + t_besier_value^3 ) 
+End Function
