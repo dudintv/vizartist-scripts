35562d6691dd17941fb1df0bae1f5d58a6acf62e
Dmitry Dudin <svetoz@gmail.com>
2019-11-28 16:54:42 +0100

    update debug-shared-memory

--- dudin-debug/dudin-debug-shared-memory/README.md
+++ dudin-debug/dudin-debug-shared-memory/README.md
@@ -1,4 +1,10 @@
 ## Debug variables in SharedMemory 
 
+#### Version 1.2
+* Print all variables at the one string.
+
+#### Version 1.1
+* Add "Print now" all considered variables. Only if they exist.
+
 #### Version 1.0
 * Can watch and print several variables changings and deleting to console and/or this.geometry.text
--- dudin-debug/dudin-debug-shared-memory/dudin-debug-shared-memory.vb
+++ dudin-debug/dudin-debug-shared-memory/dudin-debug-shared-memory.vb
@@ -1,4 +1,4 @@
-RegisterPluginVersion(1,0,0)
+RegisterPluginVersion(1,2,0)
 Dim infoText As String = "Debug global variables in all three SharedMemory hashes(Scene, System, VizCommunication). Author: Dmitry Dudin, http://dudin.tv"
 
 Dim arr_varnames As Array[String]
@@ -11,6 +11,7 @@ sub OnInitParameters()
 	RegisterParameterString("varnames", "Variable names (xxx, yyy)", "", 50, 999, "")
 	RegisterParameterBool("print_console", "Print to console", true)
 	RegisterParameterBool("print_thistext", "Print to this.text", false)
+	RegisterPushButton("print_now", "Print now (if exist)", 1)
 end sub
 
 sub OnInit()
@@ -36,23 +37,48 @@ sub OnParameterChanged(parameterName As String)
 	OnInit()
 end sub
 
-Dim hash As String
 sub OnSharedMemoryVariableChanged(map As SharedMemory, mapKey As String)
+	PrintClear()
 	Output("CHANGE", map, mapKey)
+	PrintOut()
 end sub
 
 sub OnSharedMemoryVariableDeleted(map As SharedMemory, mapKey As String)
+	PrintClear()
 	Output("DELETE", map, mapKey)
+	PrintOut()
 end sub
 
+Dim all As String
+Sub PrintClear()
+	all = ""
+End Sub
+
 Sub Output(action As String, map As SharedMemory, mapKey As String)
+	Dim hash As String
 	if map == Scene.Map then hash = "Scene"
 	if map == System.Map then hash = "System"
 	if map == VizCommunication.Map then hash = "VizCom"
+	all = all & action & " " & hash & "[" & mapKey & "] = " & CStr(map[mapKey]) & "\n"
+End Sub
+
+Sub PrintOut()
 	if GetParameterBool("print_console") then
-		println(action & " " & hash & "[" & mapKey & "] = " & CStr(map[mapKey]))
+		println(all)
 	end if
 	if GetParameterBool("print_console") then
-		this.Geometry.Text = action & " " & hash & "[" & mapKey & "] = " & CStr(map[mapKey])
+		this.Geometry.Text = all
 	end if
 End Sub
+
+sub OnExecAction(buttonId As Integer)
+	if buttonId == 1 then
+		PrintClear()
+		for i=0 to arr_varnames.ubound
+			if GetParameterBool("check_scene")  AND Scene.Map.ContainsKey(arr_varnames[i])  then Output("NOW", Scene.Map, arr_varnames[i])
+			if GetParameterBool("check_system") AND System.Map.ContainsKey(arr_varnames[i]) then Output("NOW", System.Map, arr_varnames[i])
+			if GetParameterBool("check_vizcom") AND VizCommunication.Map.ContainsKey(arr_varnames[i]) then Output("NOW", VizCommunication.Map, arr_varnames[i])
+		next
+		PrintOut()
+	end if
+end sub
