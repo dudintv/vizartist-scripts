fad5eff3a9c743687e3ef1e802fec42e8c98d5c7
Dmitry Dudin <svetoz@gmail.com>
2020-02-01 21:03:14 +0100

    add first version of omo-plugins scripts

---
+++ dudin-omo/dudin-omo-plugins/README.md
@@ -0,0 +1,6 @@
+## "OMO" for plugins
+
+Switch "active" state of child containers plugins like do classic OMO with container "active".
+
+#### Version 1.0 (01 february 2020)
+* you can type any name of plugin in interface
---
+++ dudin-omo/dudin-omo-plugins/dudin-omo-plugins.vb
@@ -0,0 +1,44 @@
+RegisterPluginVersion(1,0,0)
+Dim info As String = "
+Omo for switching certain plugins in child containers
+Developer: Dmitry Dudin.
+"
+
+Dim arr_child As Array[Container]
+Dim arr_plugin As Array[PluginInstance]
+
+sub OnInitParameters()
+	RegisterParameterString("plugin", "Plugin name", "", 40, 999, "")
+	RegisterParameterInt("num", "Number", 0, 0, 999)
+end sub
+
+sub OnInit()
+	GetFirstLevelChilds(this, arr_child)
+end sub
+
+sub OnParameterChanged(parameterName As String)
+	if parameterName == "plugin" then
+		GetPlugins()
+	elseif parameterName == "num" then
+		for i=0 to arr_plugin.ubound
+			arr_plugin[i].Active = ( GetParameterInt("num") == i )
+		next
+	end if
+end sub
+
+Sub GetPlugins()
+	arr_plugin.Clear()
+	for i=0 to arr_child.ubound
+		arr_plugin.Push( arr_child[i].GetFunctionPluginInstance(GetParameterString("plugin")) )
+	next
+End Sub
+
+Sub GetFirstLevelChilds(root as Container, ByRef _arr_childs As Array[Container])
+	_arr_childs.Clear()
+	Dim _c As Container
+	_c = this.FirstChildContainer
+	Do 
+		_arr_childs.Push(_c)
+		_c = _c.NextContainer
+	Loop While _c <> null
+End Sub
