ad1c2e73d034f80169340ac48b0e8802c88db052
Dmitry Dudin <svetoz@gmail.com>
2020-02-01 20:49:12 +0100

    x

--- dudin-container/get-firstlevel-childs.vb
+++ dudin-container/get-firstlevel-childs.vb
@@ -3,7 +3,7 @@ Sub GetFirstLevelChilds(root as Container, ByRef _arr_childs As Array[Container]
 	Dim _c As Container
 	_c = this.FirstChildContainer
 	Do 
-		_arr_childs.Push(c)
+		_arr_childs.Push(_c)
 		_c = _c.NextContainer
 	Loop While _c <> null
 End Sub
