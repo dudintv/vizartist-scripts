e3d2beae54b08b47ed9d81da2f49a4e6ac30725e
Dmitry Dudin <svetoz@gmail.com>
2020-05-05 23:24:37 +0200

    update dudin-omo-bytime

--- dudin-omo/dudin-omo-bytime/dudin-omo-bytime.vb
+++ dudin-omo/dudin-omo-bytime/dudin-omo-bytime.vb
@@ -7,19 +7,19 @@ sub OnInitParameters()
 	next
 end sub
 
-Dim timeperiodbegins As Array[Integer]
+Dim time_period_begins As Array[Integer]
 
 sub OnInit()
 	Dim s As String
 	Dim arr_s As Array[String]
-	timeperiodbegins.clear()
+	time_period_begins.clear()
 	for i=1 to count_time_periods
 		s = GetParameterString("period_begin_" & i)
 		s.Trim()
 		s.Split(":", arr_s)
 		
 		' get total count minutes from begin of the day, to make comparison much easier
-		timeperiodbegins.Push(CInt(arr_s[0])*60 + CInt(arr_s[1]))
+		time_period_begins.Push(CInt(arr_s[0])*60 + CInt(arr_s[1]))
 	next
 end sub
 sub OnParameterChanged(parameterName As String)
@@ -35,9 +35,9 @@ sub OnExecPerField()
 	current_count_minutes_from_begin_of_today = dt.Hour*60 + dt.Minute
 	
 	' lets find currect time period
-	current_period = timeperiodbegins.ubound
-	for i=timeperiodbegins.ubound to 0 step -1
-		if current_count_minutes_from_begin_of_today >= timeperiodbegins[i] then
+	current_period = time_period_begins.ubound
+	for i=time_period_begins.ubound to 0 step -1
+		if current_count_minutes_from_begin_of_today >= time_period_begins[i] then
 			current_period = i
 			exit for
 		end if
