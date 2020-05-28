1283dacd0ae61af322e63627e57a61e7eddfee8f
Dmitry Dudin <svetoz@gmail.com>
2020-05-28 11:25:47 +0200

    fix utility "find subcontainers by name"

--- dudin-container/dudin-container-find-subcontainers-by-name.vb
+++ dudin-container/dudin-container-find-subcontainers-by-name.vb
@@ -3,10 +3,11 @@ Function GetSubContainersByName(_c As Container, _name As String) As Array[Conta
 	_c.GetContainerAndSubContainers(_arr, false)
 	_arr.Erase(0)
 	for i=0 to _arr.ubound
-		if _arr[i].name <> _name then
+		if NOT _arr[i].name.Match(_name) then
 			_arr.Erase(i)
-			i += 1
+			i -= 1
 		end if
 	next
-	GetSubContainerByName = _arr
+
+	GetSubContainersByName = _arr
 End Function
