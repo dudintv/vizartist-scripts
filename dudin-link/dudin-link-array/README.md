5bdbd45bcdfe6d54d6c1279fb7dca4dea413a65d
Dmitry Dudin <svetoz@gmail.com>
2020-06-14 23:09:59 +0200

    update dudin-link-array

--- dudin-link/dudin-link-array/README.md
+++ dudin-link/dudin-link-array/README.md
@@ -1,5 +1,8 @@
 ## Link transformation for array sub-containers
 
+#### Version 1.1 (14 June 2020)
+* Watch for all sub-sub-children of target and link to target
+
 #### Version 1.0 (14 June 2020)
 * Button to make a full copy of all children from Source to Target container
 * You can limit parameters which will be synced
--- dudin-link/dudin-link-array/dudin-link-array.vb
+++ dudin-link/dudin-link-array/dudin-link-array.vb
@@ -1,6 +1,8 @@
-RegisterPluginVersion(1,0,0)
+RegisterPluginVersion(1,1,0)
 
 Dim c_source, c_target As Container
+Dim arr_source, arr_target As Array[Container]
+
 Dim new_active As Boolean
 Dim new_pos, new_rot, new_scale As Vertex
 Dim new_alpha As Double
@@ -31,21 +33,45 @@ end sub
 sub OnInit()
 	c_source = GetParameterContainer("source")
 	c_target = GetParameterContainer("target")
+	FillArrays()
 	FillPrevs()
 end sub
 sub OnParameterChanged(parameterName As String)
 	OnInit()
 end sub
 
-Sub FillPrevs()
+Sub FillArrays()
+	arr_source.Clear()
 	for i=0 to c_source.ChildContainerCount - 1
+		PushContainers(arr_source, c_source.GetChildContainerByIndex(i))
+	next
+	arr_target.Clear()
+	for i=0 to c_target.ChildContainerCount - 1
+		PushContainers(arr_target, c_target.GetChildContainerByIndex(i))
+	next
+End Sub
+
+Sub PushContainers(arr_c as Array[Container], c As Container)
+	if c.ChildContainerCount > 0 then
+		arr_c.Push(c)
+		for i=0 to c.ChildContainerCount - 1
+			PushContainers(arr_c, c.GetChildContainerByIndex(i))
+		next
+	else
+		arr_c.Push(c)
+	end if
+End Sub
+
+Sub FillPrevs()
+	arr_prev.Clear()
+	for i=0 to arr_source.ubound
 		Dim this_prev As Prev
-		this_prev.active = c_source.GetChildContainerByIndex(i).active
-		this_prev.pos    = c_source.GetChildContainerByIndex(i).position.xyz
-		this_prev.rot    = c_source.GetChildContainerByIndex(i).rotation.xyz
-		this_prev.scale  = c_source.GetChildContainerByIndex(i).scaling.xyz
-		this_prev.alpha  = c_source.GetChildContainerByIndex(i).alpha.value
-		this_prev.text   = c_source.GetChildContainerByIndex(i).geometry.text
+		this_prev.active = arr_source[i].active
+		this_prev.pos    = arr_source[i].position.xyz
+		this_prev.rot    = arr_source[i].rotation.xyz
+		this_prev.scale  = arr_source[i].scaling.xyz
+		this_prev.alpha  = arr_source[i].alpha.value
+		this_prev.text   = arr_source[i].geometry.text
 		arr_prev.Push(this_prev)
 	next
 End Sub
@@ -54,69 +80,69 @@ sub OnExecAction(buttonId As Integer)
 	if buttonId == 1 then
 		' copy
 		c_target.DeleteChildren()
-		arr_prev.Clear()
 		for i = c_source.ChildContainerCount-1 to 0 step -1
 			c_source.GetChildContainerByIndex(i).CopyTo(c_target, TL_DOWN)
 		next
+		FillArrays()
 		FillPrevs()
 		scene.UpdateSceneTree()
 	end if
 end sub
 
 sub OnExecPerField()
-	if c_source.ChildContainerCount <> c_target.ChildContainerCount then exit sub
+	if arr_source.size <> arr_target.size then exit sub
 	
-	for i=0 to c_source.ChildContainerCount - 1
+	for i=0 to arr_source.ubound
 		' active
 		if GetParameterBool("active") then
-			new_active = c_source.GetChildContainerByIndex(i).active
+			new_active = arr_source[i].active
 			if arr_prev[i].active <> new_active then
-				c_target.GetChildContainerByIndex(i).active = new_active
+				arr_target[i].active = new_active
 				arr_prev[i].active = new_active
 			end if
 		end if
 		
 		'position
 		if GetParameterBool("pos") then
-			new_pos = c_source.GetChildContainerByIndex(i).position.xyz
+			new_pos = arr_source[i].position.xyz
 			if arr_prev[i].pos <> new_pos then
-				c_target.GetChildContainerByIndex(i).position.xyz = new_pos
+				arr_target[i].position.xyz = new_pos
 				arr_prev[i].pos = new_pos
 			end if
 		end if
 		
 		'rotation
 		if GetParameterBool("rot") then
-			new_rot = c_source.GetChildContainerByIndex(i).rotation.xyz
+			new_rot = arr_source[i].rotation.xyz
 			if arr_prev[i].rot <> new_rot then
-				c_target.GetChildContainerByIndex(i).rotation.xyz = new_rot
+				arr_target[i].rotation.xyz = new_rot
 				arr_prev[i].rot = new_rot
 			end if
 		end if
 		
 		'scale
 		if GetParameterBool("scale") then
-			new_scale = c_source.GetChildContainerByIndex(i).scaling.xyz
+			new_scale = arr_source[i].scaling.xyz
 			if arr_prev[i].scale <> new_scale then
-				c_target.GetChildContainerByIndex(i).scaling.xyz = new_scale
+				arr_target[i].scaling.xyz = new_scale
 				arr_prev[i].scale = new_scale
 			end if
 		end if
 		
 		'alpha
 		if GetParameterBool("alpha") then
-			new_alpha = c_source.GetChildContainerByIndex(i).alpha.value
+			new_alpha = arr_source[i].alpha.value
 			if arr_prev[i].alpha <> new_alpha then
-				c_target.GetChildContainerByIndex(i).alpha.value = new_alpha
+				arr_target[i].alpha.value = new_alpha
 				arr_prev[i].alpha = new_alpha
 			end if
 		end if
 		
 		'text
 		if GetParameterBool("text") then
-			new_text = c_source.GetChildContainerByIndex(i).geometry.text
+			new_text = arr_source[i].geometry.text
 			if arr_prev[i].text <> new_text then
-				c_target.GetChildContainerByIndex(i).geometry.text = new_text
+				arr_target[i].geometry.text = new_text
 				arr_prev[i].text = new_text
 			end if
 		end if
