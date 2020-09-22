e7a38936db4ba07f8bdcf665e538ef7ef4d0c4db
Dmitry Dudin <svetoz@gmail.com>
2020-09-22 16:38:40 +0200

    update BG

--- dudin-bg/bg.vb
+++ dudin-bg/bg.vb
@@ -1,12 +1,14 @@
-Dim info As String = "В случае использования для текст_с_подложкой скрипт кидается на ТЕКСТовый контейнер!
-Скрипт постоянно измеряет размеры self-контейнера/потомков и изменяет scaling указанного контейнера по X и Y.
-Изменения scaling происходят анимационно с замедлением в конце (см. конец скрипта)
-Разработчик: Дудин Дмитрий. Версия 1.6 (07 сентября 2015)
+RegisterPluginVersion(1,7,0)
+
+Dim info As String = "Drop it on text container in case using with text+bg composition.
+The script continuosly measuring bounding box of this container and apply calculation on choosen container.
+Changing happens with delay. Look at the end of the script.
+Developer: Dmitry Dudin, dudin.tv
 "
  
-Dim fon, child, c_min_x, c_min_y As Container
-Dim mode_size, mode, mode_min_x, mode_min_y, mode_shange_fon As Integer
-Dim x,y, x_multy, y_multy, x_padding, y_padding, x_min, y_min As Double
+Dim fon, child, c_min_x, c_min_y, c_min_z As Container
+Dim mode_size, mode, mode_min_x, mode_min_y, mode_min_z, mode_shange_fon As Integer
+Dim x,y,z, x_multy, y_multy, z_multy, x_padding, y_padding, z_padding, x_min, y_min, z_min As Double
 Dim pos As Vertex
 Dim size, child_size, min_size As Vertex
 Dim newSize, v1, v2, vthis1, vthis2 As Vertex
@@ -17,7 +19,14 @@ Dim sizeTreshold, animTreshold As Double
 Dim arr_s As Array[String]
 arr_s.Push("X")
 arr_s.Push("Y")
+arr_s.Push("Z")
 arr_s.Push("XY")
+
+Dim MODE_X  As Integer = 0
+Dim MODE_Y  As Integer = 1
+Dim MODE_Z  As Integer = 2
+Dim MODE_XY As Integer = 3
+
 Dim arr_ss As Array[String]
 arr_ss.Push("Общий размер")
 arr_ss.Push("Макс. из потомков")
@@ -38,16 +47,24 @@ sub OnInitParameters()
 	RegisterParameterInt("num_child", "└ Номер потомка (-1=никакой)", 0, -1, 100)
 	RegisterRadioButton("mode", "└ Какие размеры учитывать:", 0, arr_s)
 	RegisterParameterDouble("x_multy", "   └ Множитель по X", 1.0, 0.0, 10000000.0)
-	RegisterParameterDouble("y_multy", "   └ Множитель по Y", 1.0, 0.0, 10000000.0)
 	RegisterParameterDouble("x_padding", "   └ Отступ по X", 0.0, -100000.0, 10000000.0)
+	RegisterParameterDouble("y_multy", "   └ Множитель по Y", 1.0, 0.0, 10000000.0)
 	RegisterParameterDouble("y_padding", "   └ Отступ по Y", 0.0, -100000.0, 10000000.0)
+	RegisterParameterDouble("z_multy", "   └ Множитель по Z", 1.0, 0.0, 10000000.0)
+	RegisterParameterDouble("z_padding", "   └ Отступ по Z", 0.0, -100000.0, 10000000.0)
 	
 	RegisterRadioButton("x_min_mode", "Минимальный фон по X", 0, arr_sss)
 	RegisterParameterDouble("x_min", "└ Мин. X", 0.0, 0.0, 10000000.0)
 	RegisterParameterContainer("x_min_c", "└ Мин. X")
+	
 	RegisterRadioButton("y_min_mode", "Минимальный фон по Y", 0, arr_sss)
 	RegisterParameterDouble("y_min", "└ Мин. Y", 0.0, 0.0, 10000000.0)
 	RegisterParameterContainer("y_min_c", "└ Мин. Y")
+	
+	RegisterRadioButton("z_min_mode", "Минимальный фон по Z", 0, arr_sss)
+	RegisterParameterDouble("z_min", "└ Мин. Z", 0.0, 0.0, 10000000.0)
+	RegisterParameterContainer("z_min_c", "└ Мин. Z")
+	
 	RegisterParameterBool("hide_by_zero", "Скрыть фон если нул.размер", TRUE)
 	RegisterParameterDouble("treshold", "└ Типа-нулевой размер this", 0.1, 0.0, 1000.0)
 	RegisterParameterDouble("inertion", "Инерция анимации", 2.0, 1.0, 100.0)
@@ -69,45 +86,75 @@ sub OnGuiStatus()
 	End If
  
 	mode = GetParameterInt("mode")
-	If mode == 0 Then
-		'X
+	
+	Select Case mode
+	Case MODE_X
 		SendGuiParameterShow("x_multy",SHOW)
 		SendGuiParameterShow("y_multy",HIDE)
+		SendGuiParameterShow("z_multy",HIDE)
 		SendGuiParameterShow("x_padding",SHOW)
 		SendGuiParameterShow("y_padding",HIDE)
+		SendGuiParameterShow("z_padding",HIDE)
 		SendGuiParameterShow("x_min_mode",SHOW)
 		SendGuiParameterShow("x_min",SHOW)
 		SendGuiParameterShow("x_min_c",SHOW)
 		SendGuiParameterShow("y_min_mode",HIDE)
 		SendGuiParameterShow("y_min",HIDE)
 		SendGuiParameterShow("y_min_c",HIDE)
-	ElseIf mode == 1 Then
-		'Y
+		SendGuiParameterShow("z_min_mode",HIDE)
+		SendGuiParameterShow("z_min",HIDE)
+		SendGuiParameterShow("z_min_c",HIDE)
+	Case MODE_Y
 		SendGuiParameterShow("x_multy",HIDE)
 		SendGuiParameterShow("y_multy",SHOW)
+		SendGuiParameterShow("z_multy",HIDE)
 		SendGuiParameterShow("x_padding",HIDE)
 		SendGuiParameterShow("y_padding",SHOW)
+		SendGuiParameterShow("z_padding",HIDE)
 		SendGuiParameterShow("x_min_mode",HIDE)
 		SendGuiParameterShow("x_min",HIDE)
 		SendGuiParameterShow("x_min_c",HIDE)
 		SendGuiParameterShow("y_min_mode",SHOW)
 		SendGuiParameterShow("y_min",SHOW)
 		SendGuiParameterShow("y_min_c",SHOW)
-	ElseIf mode == 2 Then
-		'XY
+		SendGuiParameterShow("z_min_mode",HIDE)
+		SendGuiParameterShow("z_min",HIDE)
+		SendGuiParameterShow("z_min_c",HIDE)
+	Case MODE_Z
+		SendGuiParameterShow("x_multy",HIDE)
+		SendGuiParameterShow("y_multy",HIDE)
+		SendGuiParameterShow("z_multy",SHOW)
+		SendGuiParameterShow("x_padding",HIDE)
+		SendGuiParameterShow("y_padding",HIDE)
+		SendGuiParameterShow("z_padding",SHOW)
+		SendGuiParameterShow("x_min_mode",HIDE)
+		SendGuiParameterShow("x_min",HIDE)
+		SendGuiParameterShow("x_min_c",HIDE)
+		SendGuiParameterShow("y_min_mode",SHOW)
+		SendGuiParameterShow("y_min",HIDE)
+		SendGuiParameterShow("y_min_c",HIDE)
+		SendGuiParameterShow("z_min_mode",SHOW)
+		SendGuiParameterShow("z_min",SHOW)
+		SendGuiParameterShow("z_min_c",SHOW)
+	Case MODE_XY
 		SendGuiParameterShow("x_multy",SHOW)
 		SendGuiParameterShow("y_multy",SHOW)
+		SendGuiParameterShow("z_multy",HIDE)
 		SendGuiParameterShow("x_padding",SHOW)
 		SendGuiParameterShow("y_padding",SHOW)
+		SendGuiParameterShow("z_padding",HIDE)
 		SendGuiParameterShow("x_min_mode",SHOW)
 		SendGuiParameterShow("x_min",SHOW)
 		SendGuiParameterShow("x_min_c",SHOW)
 		SendGuiParameterShow("y_min_mode",SHOW)
 		SendGuiParameterShow("y_min",SHOW)
 		SendGuiParameterShow("y_min_c",SHOW)
-	End If
+		SendGuiParameterShow("z_min_mode",HIDE)
+		SendGuiParameterShow("z_min",HIDE)
+		SendGuiParameterShow("z_min_c",HIDE)
+	End Select
 	
-	If mode == 0 OR mode == 2 Then
+	If mode == MODE_X OR mode == MODE_XY Then
 		mode_min_x = GetParameterInt("x_min_mode")
 		If mode_min_x == 0 Then
 			SendGuiParameterShow("x_min",SHOW)
@@ -118,7 +165,7 @@ sub OnGuiStatus()
 		End If
 	End If
 	
-	If mode == 1 OR mode == 2 Then
+	If mode == MODE_Y OR mode == MODE_XY Then
 		mode_min_y = GetParameterInt("y_min_mode")
 		If mode_min_y == 0 Then
 			SendGuiParameterShow("y_min",SHOW)
@@ -126,6 +173,17 @@ sub OnGuiStatus()
 		ElseIf mode_min_y == 1 Then
 			SendGuiParameterShow("y_min",HIDE)
 			SendGuiParameterShow("y_min_c",SHOW)
+		End If
+	End If
+	
+	If mode == MODE_Z Then
+		mode_min_z = GetParameterInt("z_min_mode")
+		If mode_min_z == 0 Then
+			SendGuiParameterShow("z_min",SHOW)
+			SendGuiParameterShow("z_min_c",HIDE)
+		ElseIf mode_min_z == 1 Then
+			SendGuiParameterShow("z_min",HIDE)
+			SendGuiParameterShow("z_min_c",SHOW)
 		End If
 	End If
 	
@@ -181,10 +239,13 @@ sub OnExecPerField()
  
 	x_multy = GetParameterDouble("x_multy")
 	y_multy = GetParameterDouble("y_multy")
+	z_multy = GetParameterDouble("z_multy")
 	x_padding = GetParameterDouble("x_padding")
 	y_padding = GetParameterDouble("y_padding")
+	z_padding = GetParameterDouble("z_padding")
 	c_min_x = GetParameterContainer("x_min_c")
 	c_min_y = GetParameterContainer("y_min_c")
+	c_min_z = GetParameterContainer("z_min_c")
 	
 	If mode_min_x == 1 Then
 		'надо брать минимальный размер у контейнера
@@ -224,10 +285,9 @@ sub OnExecPerField()
 	
 	
 	'-------------------------------------------------------------------------------------------------
-	'обработаем ширину
-	If mode == 0 OR mode == 2 Then
+	'processing X
+	If mode == MODE_X OR mode == MODE_XY Then
 		If size.X < sizeTreshold Then
-			'если ширина отсутсвует то скрыть фон
 			If GetParameterBool("hide_by_zero") Then
 				fon.Active = false
 				exit sub
@@ -243,10 +303,9 @@ sub OnExecPerField()
 		End If
 	End If
  
-	'обработаем высоту
-	If mode == 1 OR mode == 2 Then
+	'processing Y
+	If mode == MODE_Y OR mode == MODE_XY Then
 		If size.Y < sizeTreshold Then
-			'если высота отсутсвует то скрыть фон
 			If GetParameterBool("hide_by_zero") Then
 				fon.Active = false
 				exit sub
@@ -262,8 +321,26 @@ sub OnExecPerField()
 		End If
 	End If
 	
+	'processing Z
+	If mode == MODE_Z Then
+		If size.Z < sizeTreshold Then
+			If GetParameterBool("hide_by_zero") Then
+				fon.Active = false
+				exit sub
+			Else
+				fon.Active = true
+				newSize.Z = 0
+			End If
+		Else
+			fon.Active = true
+			z = size.Z/100.0 * z_multy + z_padding/100.0
+			If z < z_min Then z = z_min
+			newSize.Z = z
+		End If
+	End If
+	
 	'-------------------------------------------------------------------------------------------------
-	'позиционирование
+	'positioning
 	if GetParameterBool("position_y") then
 		this.GetTransformedBoundingBox(vthis1,vthis2)
 		vthis2 = this.LocalPosToWorldPos(vthis2)
@@ -274,17 +351,17 @@ sub OnExecPerField()
  	
  
 	'-------------------------------------------------------------------------------------------------
-	'анимация и реальное изменение размеров
+	'animation & real size changing
  
 	animTreshold = 0.001
 	
 	if mode_shange_fon == 1 then
 		newSize.x = 100*newSize.x
 		newSize.y = 100*newSize.y
+		newSize.z = 100*newSize.z
 	end if
 	
-	if mode == 0 OR mode == 2 Then
-		'X
+	if mode == MODE_X OR mode == MODE_XY Then
 		if mode_shange_fon == 0 then
 			if newSize.x<(fon.scaling.x-animTreshold) OR newSize.x>(fon.scaling.x+animTreshold) Then
 				fon.scaling.x -= (fon.scaling.x - newSize.x)/GetParameterDouble("inertion")
@@ -299,8 +376,7 @@ sub OnExecPerField()
 			end if
 		end if
 	end if
-	if mode == 1 OR mode == 2 Then
-		'Y
+	if mode == MODE_Y OR mode == MODE_XY Then
 		if mode_shange_fon == 0 then
 			if newSize.y<(fon.scaling.y-animTreshold) OR newSize.y>(fon.scaling.y+animTreshold) Then
 				fon.scaling.y -= (fon.scaling.y - newSize.y)/GetParameterDouble("inertion")
@@ -312,6 +388,21 @@ sub OnExecPerField()
 				fon.geometry.SetParameterDouble(  "height", fon.geometry.GetParameterDouble("height") - (fon.geometry.GetParameterDouble("height") - newSize.y)/GetParameterDouble("inertion")  )
 			else
 				fon.geometry.SetParameterDouble(  "height", newSize.y  )
+			end if
+		end if
+	end if
+	if mode == MODE_Z Then
+		if mode_shange_fon == 0 then
+			if newSize.z<(fon.scaling.z-animTreshold) OR newSize.z>(fon.scaling.z+animTreshold) Then
+				fon.scaling.z -= (fon.scaling.z - newSize.z)/GetParameterDouble("inertion")
+			else
+				fon.scaling.z = newSize.z
+			end if
+		elseif mode_shange_fon == 1 then
+			if newSize.z<(fon.geometry.GetParameterDouble("height")-animTreshold) OR newSize.y>(fon.geometry.GetParameterDouble("height")+animTreshold) Then
+				fon.geometry.SetParameterDouble(  "height", fon.geometry.GetParameterDouble("height") - (fon.geometry.GetParameterDouble("height") - newSize.z)/GetParameterDouble("inertion")  )
+			else
+				fon.geometry.SetParameterDouble(  "height", newSize.z  )
 			end if
 		end if
 	end if
