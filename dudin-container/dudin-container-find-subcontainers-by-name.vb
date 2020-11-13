5cd25e4d691e4a6a29600f761781024b365408e6
Dmitry Dudin <svetoz@gmail.com>
2020-11-13 15:02:38 +0100

    update container tool to find dub-sontainer by matching name

--- dudin-container/dudin-container-find-subcontainers-by-name.vb
+++ dudin-container/dudin-container-find-subcontainers-by-name.vb
@@ -1,13 +1,12 @@
-Function GetSubContainersByName(_c As Container, _name As String) As Array[Container]
-	Dim _arr As Array[Container]
-	_c.GetContainerAndSubContainers(_arr, false)
-	_arr.Erase(0)
-	for i=0 to _arr.ubound
-		if NOT _arr[i].name.Match(_name) then
-			_arr.Erase(i)
-			i -= 1
+Function GetChildrenByNameContainsString(_parent As Container, _name As String) As Array[Container]
+	Dim _arr_childs As Array[Container]
+	_parent.GetContainerAndSubContainers(_arr_childs, false)
+	_arr_childs.Erase(0)
+	for _i=0 to _arr_childs.ubound
+		if NOT _arr_childs[_i].name.Match(_name) then
+			_arr_childs.Erase(_i)
+			_i -= 1
 		end if
 	next
-
-	GetSubContainersByName = _arr
+	GetChildrenByNameContainsString = _arr_childs
 End Function
