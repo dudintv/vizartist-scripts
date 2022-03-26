29a940c3ef73a2b526749ff9277219fef6bb9d95
Dmitriy <svetoz@gmail.com>
2022-03-26 13:02:48 +0100

    fix CollectDataFromArray function

--- dudin-parser/dudin-parser-json-native/dudin-parser-json-native.vb
+++ dudin-parser/dudin-parser-json-native/dudin-parser-json-native.vb
@@ -7,28 +7,30 @@ Sub Parse()
 End Sub
 
 Function CollectDataFromArray(_j As Json, keys As String) as String
+	Dim _s As String
 	Dim _arr_keys As Array[String]
 	keys.Split("|", _arr_keys)
 	
-	Dim arr_s, arr_line As Array[String]
+	Dim _arr_s, _arr_line As Array[String]
 	
-	arr_s.Clear()
+	_arr_s.Clear()
 	for i=0 to _j.size-1
-		arr_line.Clear()
-		for k=0 to arr_keys.ubound
+		_arr_line.Clear()
+		for k=0 to _arr_keys.ubound
 			if _j.At(i)[_arr_keys[k]].IsString() then
-				arr_line.Push( _j.At(i).GetString(_arr_keys[k]) )
+				_arr_line.Push( _j.At(i).GetString(_arr_keys[k]) )
 			elseif _j.At(i)[_arr_keys[k]].IsBoolean() then
-				arr_line.Push(CStr(_j.At(i).GetBoolean(_arr_keys[k])))
+				_arr_line.Push(CStr(_j.At(i).GetBoolean(_arr_keys[k])))
 			elseif _j.At(i)[_arr_keys[k]].IsDouble() then
-				arr_line.Push(DoubleToString(_j.At(i).GetDouble(_arr_keys[k]), 2))
+				_arr_line.Push(DoubleToString(_j.At(i).GetDouble(_arr_keys[k]), 1))
 			elseif _j.At(i)[_arr_keys[k]].IsInteger() then
-				arr_line.Push(CStr(_j.At(i).GetInteger(_arr_keys[k])))
+				_arr_line.Push(CStr(_j.At(i).GetInteger(_arr_keys[k])))
 			end if
 		next
-		s.Join(arr_line, "|")
-		arr_s.Push(s)
+		_s.Join(_arr_line, "|")
+		_arr_s.Push(_s)
 	next
-	s.Join(arr_s, "\n")
-	CollectDataFromArray = s
+	
+	_s.Join(_arr_s, "\n")
+	CollectDataFromArray = _s
 End Function
