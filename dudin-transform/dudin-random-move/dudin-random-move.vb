cc374bfb7b332b69de180ba2a3919bfa358aa7e1
Dmitriy <svetoz@gmail.com>
2022-09-02 12:16:35 +0200

    change code casing

--- dudin-transform/dudin-random-move/dudin-random-move.vb
+++ dudin-transform/dudin-random-move/dudin-random-move.vb
@@ -1,56 +1,56 @@
-Dim pos_x, pos_y, pos_z, pos_speed As Double
-Dim max_x, max_y, max_z, max_speed As Double
-Dim x, y, z, speed_x, speed_y, speed_z As Double
-Dim dir_x, dir_y, dir_z As Integer
+Dim posX, posY, posZ, posSpeed As Double
+Dim maxX, maxY, maxZ, maxSpeed As Double
+Dim x, y, z, speedX, speedY, speedZ As Double
+Dim dirX, dirY, dirZ As Integer
 
 sub OnInitParameters()
-	RegisterParameterDouble("pos_x", "X amplitude", 0, 0, 999999)
-	RegisterParameterDouble("pos_y", "Y amplitude", 0, 0, 999999)
-	RegisterParameterDouble("pos_z", "Z amplitude", 0, 0, 999999)
-	RegisterParameterDouble("pos_speed", "Position Speed", 0, 0, 999999)
+	RegisterParameterDouble("posX", "X amplitude", 0, 0, 999999)
+	RegisterParameterDouble("posY", "Y amplitude", 0, 0, 999999)
+	RegisterParameterDouble("posZ", "Z amplitude", 0, 0, 999999)
+	RegisterParameterDouble("posSpeed", "Position Speed", 0, 0, 999999)
 end sub
 
 sub OnInit()
-	pos_x = GetParameterDouble("pos_x")
-	pos_y = GetParameterDouble("pos_y")
-	pos_z = GetParameterDouble("pos_z")
-	pos_speed = GetParameterDouble("pos_speed")
+	posX = GetParameterDouble("posX")
+	posY = GetParameterDouble("posY")
+	posZ = GetParameterDouble("posZ")
+	posSpeed = GetParameterDouble("posSpeed")
 	x = 0
 	y = 0
 	z = 0
-	speed_x = (pos_speed/2.0 + (pos_speed/2.0)*Random())/100.0
-	speed_y = (pos_speed/2.0 + (pos_speed/2.0)*Random())/100.0
-	speed_z = (pos_speed/2.0 + (pos_speed/2.0)*Random())/100.0
-	dir_x = 1
-	dir_y = 1
-	dir_z = 1
+	speedX = (posSpeed/2.0 + (posSpeed/2.0)*Random())/100.0
+	speedY = (posSpeed/2.0 + (posSpeed/2.0)*Random())/100.0
+	speedZ = (posSpeed/2.0 + (posSpeed/2.0)*Random())/100.0
+	dirX = 1
+	dirY = 1
+	dirZ = 1
 end sub
 sub OnParameterChanged(parameterName As String)
 	OnInit()
 end sub
 
 sub OnExecPerField()
-	if pos_x > 0 then
-		if x >= 100 then dir_x = -1
-		if x <= 0 then dir_x = 1
-		x += dir_x*speed_x
+	if posX > 0 then
+		if x >= 100 then dirX = -1
+		if x <= 0 then dirX = 1
+		x += dirX*speedX
 	end if
 	
-	if pos_y > 0 then
-		if y >= 100 then dir_y = -1
-		if y <= 0 then dir_y = 1
-		y += dir_y*speed_y
+	if posY > 0 then
+		if y >= 100 then dirY = -1
+		if y <= 0 then dirY = 1
+		y += dirY*speedY
 	end if
 
-	if pos_z > 0 then	
-		if z >= 100 then dir_z = -1
-		if z <= 0 then dir_z = 1
-		z += dir_z*speed_z
+	if posZ > 0 then	
+		if z >= 100 then dirZ = -1
+		if z <= 0 then dirZ = 1
+		z += dirZ*speedZ
 	end if
 	
-	this.position.x = pos_x * (Besizer(x, 0, 1, 50, 50) - 0.5)
-	this.position.y = pos_y * (Besizer(y, 0, 1, 50, 50) - 0.5)
-	this.position.z = pos_z * (Besizer(z, 0, 1, 50, 50) - 0.5)
+	this.position.x = posX * (Besizer(x, 0, 1, 50, 50) - 0.5)
+	this.position.y = posY * (Besizer(y, 0, 1, 50, 50) - 0.5)
+	this.position.z = posZ * (Besizer(z, 0, 1, 50, 50) - 0.5)
 end sub
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
@@ -63,17 +63,17 @@ Function ClampDbl(value as double, min  as double, max as double) as Double
 End Function
 
 'BESIER
-Function Besizer(ByVal procent as double, ByVal begin_value as double, ByVal end_value as double, ByVal begin_weight as double, ByVal end_weight as double) as Double
-	Dim a, b, c, d, t_besier_value As Double
+Function Besizer(ByVal procent as double, ByVal beginValue as double, ByVal end_value as double, ByVal beginWeight as double, ByVal endWeight as double) as Double
+	Dim a, b, c, d, tBesierValue As Double
 	procent      = ClampDbl(procent,       0, 100)/100.0
-	begin_weight = ClampDbl(begin_weight, 35, 100)/100.0
-	end_weight   = ClampDbl(end_weight,   35, 100)/100.0
+	beginWeight = ClampDbl(beginWeight, 35, 100)/100.0
+	endWeight   = ClampDbl(endWeight,   35, 100)/100.0
 	
-	a = 3*begin_weight - 3*(1.0 - end_weight) + 1
-	b = - 6*begin_weight + 3*(1.0 - end_weight)
-	c = 3*begin_weight
+	a = 3*beginWeight - 3*(1.0 - endWeight) + 1
+	b = - 6*beginWeight + 3*(1.0 - endWeight)
+	c = 3*beginWeight
 	d = -procent
 	
-	t_besier_value = (sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)/(3*2^(1.0/3)*a) - (2^(1.0/3)*(3*a*c - b^2))/(3*a*(sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)) - b/(3*a)
-	Besizer = begin_value + (end_value - begin_value)*( 3*(1-t_besier_value)*t_besier_value^2 + t_besier_value^3 ) 
+	tBesierValue = (sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)/(3*2^(1.0/3)*a) - (2^(1.0/3)*(3*a*c - b^2))/(3*a*(sqrt((-27*a^2*d + 9*a*b*c - 2*b^3)^2 + 4*(3*a*c - b^2)^3) - 27*a^2*d + 9*a*b*c - 2*b^3)^(1.0/3)) - b/(3*a)
+	Besizer = beginValue + (end_value - beginValue)*( 3*(1-tBesierValue)*tBesierValue^2 + tBesierValue^3 ) 
 End Function
