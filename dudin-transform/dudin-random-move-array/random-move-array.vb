73e95524991dd7d6420d632ffbf1dafb17dee645
Dmitriy <svetoz@gmail.com>
2022-09-02 20:12:32 +0200

    make `random-move-array` working with sub-children
    and along all three axis

--- dudin-transform/dudin-random-move-array/random-move-array.vb
+++ dudin-transform/dudin-random-move-array/random-move-array.vb
@@ -1,33 +1,40 @@
-Dim version As String = "1.1 (2 September 2021)"
+Dim version As String = "1.2 (2 September 2021)"
 Dim info As String = "Oscillates direct children only by Y axis"
 
-Dim speed, amplitude, random As Double
-Dim rand_amplitudes, cur_amplitudes As Array[Double]
+Dim speed, amplitudeX, amplitudeY, amplitudeZ, random As Double
+Dim randAmplitudesX, randAmplitudesY, randAmplitudesZ As Array[Double]
+Dim curAmplitudesX, curAmplitudesY, curAmplitudesZ As Array[Double]
 Dim ticks As Array[Integer]
 Dim speeds As Array[Double]
 Dim childs As Array[Container]
 
 sub OnInitParameters()
 	RegisterInfoText(version & "\n" & info)
-	RegisterParameterDouble("amplitude", "Amplitude", 1, 0, 9999)
-	RegisterParameterDouble("speed", "Speed", 50, 0, 9999)
+	RegisterParameterString("childName", "Name of sub-child", "", 100, 999, "")
+	RegisterParameterDouble("amplitudeX", "X Amplitude", 1, 0, 9999)
+	RegisterParameterDouble("amplitudeY", "Y Amplitude", 1, 0, 9999)
+	RegisterParameterDouble("amplitudeZ", "Z Amplitude", 1, 0, 9999)
+	RegisterParameterDouble("speed", "Loop duration", 50, 0, 9999)
 	RegisterParameterDouble("random", "Random level", 50, 0, 100)
 end sub
 
 sub OnInit()
-	amplitude = GetParameterDouble("amplitude")
+	amplitudeX = GetParameterDouble("amplitudeX")
+	amplitudeY = GetParameterDouble("amplitudeY")
+	amplitudeZ = GetParameterDouble("amplitudeZ")
 	speed = GetParameterDouble("speed")
 	random = GetParameterDouble("random")
 	
 	childs.Clear()
-	for i=1 to this.ChildContainerCount
-		childs.Push(this.GetChildContainerByIndex(i))
+	for i=0 to this.ChildContainerCount
+		if GetParameterString("childName").Length > 0 then
+			childs.Push(this.GetChildContainerByIndex(i).FindSubContainer(GetParameterString("childName")))
+		else
+			childs.Push(this.GetChildContainerByIndex(i))
+		end if
 	next
 	
-	SetAllRandom()
-end sub
-sub OnGeometryChanged(geom As Geometry)
-	OnInit()
+	SetupAllRandom()
 end sub
 sub OnParameterChanged(parameterName As String)
 	OnInit()
@@ -40,26 +47,40 @@ sub OnExecPerField()
 			SetRandom(i)
 		end if
 		ticks[i] += 1
-		cur_amplitudes[i] = cur_amplitudes[i] + (rand_amplitudes[i] - cur_amplitudes[i])*0.01
-		childs[i].Position.Y = cur_amplitudes[i] * Sin( 2*3.14*ticks[i]/speeds[i] )
+		curAmplitudesX[i] = curAmplitudesX[i] + (randAmplitudesX[i] - curAmplitudesX[i])*0.01
+		curAmplitudesY[i] = curAmplitudesY[i] + (randAmplitudesY[i] - curAmplitudesY[i])*0.01
+		curAmplitudesZ[i] = curAmplitudesZ[i] + (randAmplitudesZ[i] - curAmplitudesZ[i])*0.01
+		childs[i].Position.X = curAmplitudesX[i] * Sin( 2*3.14*ticks[i]/speeds[i] )
+		childs[i].Position.Y = curAmplitudesY[i] * Sin( 2*3.14*ticks[i]/speeds[i] )
+		childs[i].Position.Z = curAmplitudesZ[i] * Sin( 2*3.14*ticks[i]/speeds[i] )
 	next
 end sub
 
 
-Sub SetAllRandom()
-	rand_amplitudes.Clear()
-	cur_amplitudes.Clear()
+Sub SetupAllRandom()
+	randAmplitudesX.Clear()
+	randAmplitudesY.Clear()
+	randAmplitudesZ.Clear()
+	curAmplitudesX.Clear()
+	curAmplitudesY.Clear()
+	curAmplitudesZ.Clear()
 	ticks.Clear()
 	for i=0 to childs.UBound
 		ticks.Push(0)
 		speeds.Push(speed)
-		rand_amplitudes.Push(0)
+		randAmplitudesX.Push(0)
+		randAmplitudesY.Push(0)
+		randAmplitudesZ.Push(0)
 		SetRandom(i)
-		cur_amplitudes.Push(rand_amplitudes[i])
+		curAmplitudesX.Push(randAmplitudesX[i])
+		curAmplitudesY.Push(randAmplitudesY[i])
+		curAmplitudesZ.Push(randAmplitudesZ[i])
 	next
 End Sub
 
 Sub SetRandom(num as integer)
-	rand_amplitudes[num] = amplitude*(100.0-random)/100.0 + (amplitude*random/100.0)*Random()
+	randAmplitudesX[num] = amplitudeX*(100.0-random)/100.0 + (amplitudeX*random/100.0)*Random()
+	randAmplitudesY[num] = amplitudeY*(100.0-random)/100.0 + (amplitudeY*random/100.0)*Random()
+	randAmplitudesZ[num] = amplitudeZ*(100.0-random)/100.0 + (amplitudeZ*random/100.0)*Random()
 	speeds[num] = speed*(100.0-random)/100.0 + (speed*random/100.0)*Random()
 End Sub
