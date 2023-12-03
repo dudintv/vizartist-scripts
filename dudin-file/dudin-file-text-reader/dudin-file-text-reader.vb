29b428fd89474e1a7cba301c458cefc6de339d4b
Dmitriy <svetoz@gmail.com>
2023-12-03 17:28:34 +0100

    Update dudin-file-text-reader.vb

--- dudin-file/dudin-file-text-reader/dudin-file-text-reader.vb
+++ dudin-file/dudin-file-text-reader/dudin-file-text-reader.vb
@@ -1,45 +1,81 @@
-RegisterPluginVersion(1,2,0)
+RegisterPluginVersion(1,3,0)
 Dim info As String = "Read and normalize a text file periodically. 
 Developer: Dmitry Dudin, http://dudin.tv"
 
-Dim filepath, result, varname, s As String
-Dim arr_result As Array[String]
+Structure FrontMatterField
+	name As String
+	value As String
+End Structure
+Dim field As FrontMatterField
+Dim frontMatterFields As Array[FrontMatterField]
+
+Dim filepath, originalFullText, trimmedFullText, dataText, frontMatterText, shmDataVarName, shmFrontMatterVarName, frontMatterPrefix, s As String
+Dim arrOriginalLines, arrFrontMatterLines, arrDataLines As Array[String]
 Dim interval As Double
-Dim tick As Integer
-Dim isLoaded As Boolean
-Dim cTarget As Container
+Dim tick, frontMatterLineIndex As Integer
+Dim isLoaded, hasFrontMatter As Boolean
+Dim cDataTarget, cFrontMatterDataTarget As Container
 
 ' INTERFACE
-Dim arrOutputModes As Array[String]
-arrOutputModes.Push("This")
-arrOutputModes.Push("Other")
-arrOutputModes.Push("SHM system")
-Dim OUTPUT_TO_THIS_GEOM As Integer = 0
-Dim OUTPUT_TO_OTHER_GEOM As Integer = 1
-Dim OUTPUT_TO_SYSTEM_VAR As Integer = 2
+Dim arrDataOutputModes As Array[String]
+arrDataOutputModes.Push("This")
+arrDataOutputModes.Push("Other")
+arrDataOutputModes.Push("SHM")
+Dim OUTPUT_DATA_TO_THIS_GEOM As Integer = 0
+Dim OUTPUT_DATA_TO_OTHER_GEOM As Integer = 1
+Dim OUTPUT_DATA_TO_SYSTEM_VAR As Integer = 2
+
+Dim arrFrontMatterModes As Array[String]
+arrFrontMatterModes.Push("This text")
+arrFrontMatterModes.Push("Other text")
+arrFrontMatterModes.Push("Containers (by fields names)")
+arrFrontMatterModes.Push("One SHM variable")
+arrFrontMatterModes.Push("Distribute to SHM variables")
+Dim OUTPUT_FM_TO_THIS As Integer = 0
+Dim OUTPUT_FM_TO_OTHER As Integer = 1
+Dim OUTPUT_FM_TO_CONTAINERS As Integer = 2
+Dim OUTPUT_FM_TO_SHM As Integer = 3
+Dim OUTPUT_FM_TO_SHMS As Integer = 4
 
 sub OnInitParameters()
 	RegisterInfoText(info)
 	RegisterParameterString("filepath", "Text file fullpath", "", 100, 999, "")
 	RegisterParameterBool("is_removing_empty_lines", "Remove empty lines", false)
+	RegisterParameterBool("ignore_first_data_line", "Ignore first data line (can be a header)", false)
+
+	RegisterParameterBool("has_front_matter", "Enable Front Matter (---)", false)
+	RegisterRadioButton("front_matter_data_output_mode", " └ Output to:", OUTPUT_FM_TO_SHM, arrFrontMatterModes)
+	RegisterParameterString("front_matter_prefix", "     └ Prefix for SHM variables names", "", 100, 999, "")
+	RegisterParameterString("front_matter_shm_var_name", "     └ SHM system variable name", "", 100, 999, "")
+	RegisterParameterContainer("front_matter_target", "     └ Container with text")
+	
 	RegisterParameterDouble("interval", "Reading interval (sec)", 10.0, 1.0, 60.0)
-	RegisterRadioButton("output_mode", "Output to:", OUTPUT_TO_THIS_GEOM, arrOutputModes)
-	RegisterParameterContainer("target", "└ Text container (or this)")
-	RegisterParameterString("varname", "└ SHM system variable name", "", 100, 999, "")
+	RegisterRadioButton("data_output_mode", "Output data to:", OUTPUT_DATA_TO_THIS_GEOM, arrDataOutputModes)
+	RegisterParameterContainer("target", " └ Container with text (or this)")
+	RegisterParameterString("shm_data_var_name", " └ SHM system variable name", "", 100, 999, "")
 	RegisterPushButton("read", "Read the file now", 1)
 end sub
 
 sub OnInit()
 	filepath = GetParameterString("filepath")
 	filepath.trim()
+	shmDataVarName = GetParameterString("shm_data_var_name")
+	shmDataVarName.trim()
+	shmFrontMatterVarName = GetParameterString("front_matter_shm_var_name")
+	shmFrontMatterVarName.trim()
+	frontMatterPrefix = GetParameterString("front_matter_prefix")
+	frontMatterPrefix.trim()
+	
 	interval = GetParameterDouble("interval")
-	varname = GetParameterString("varname")
+	hasFrontMatter = GetParameterBool("has_front_matter")
 
-	if GetParameterInt("output_mode") == OUTPUT_TO_OTHER_GEOM then
-		cTarget = GetParameterContainer("target")
+	if GetParameterInt("front_matter_data_output_mode") == OUTPUT_FM_TO_OTHER then
+		cFrontMatterDataTarget = GetParameterContainer("front_matter_target")
+	end if
+	if GetParameterInt("data_output_mode") == OUTPUT_DATA_TO_OTHER_GEOM then
+		cDataTarget = GetParameterContainer("target")
 	end if
 
-	varname.trim()
 	tick = 0
 	ReadFile()
 end sub
@@ -47,8 +83,14 @@ sub OnParameterChanged(parameterName As String)
 	OnInit()
 	ReadFile()
 	
-	SendGuiParameterShow("target", CInt(GetParameterInt("output_mode") == OUTPUT_TO_OTHER_GEOM))
-	SendGuiParameterShow("varname", CInt(GetParameterInt("output_mode") == OUTPUT_TO_SYSTEM_VAR))
+	SendGuiParameterShow("target", CInt(GetParameterInt("data_output_mode") == OUTPUT_DATA_TO_OTHER_GEOM))
+	SendGuiParameterShow("shm_data_var_name", CInt(GetParameterInt("data_output_mode") == OUTPUT_DATA_TO_SYSTEM_VAR))
+	
+	Dim hasFrontMatterPrefix = (GetParameterInt("front_matter_data_output_mode") == OUTPUT_FM_TO_CONTAINERS) OR (GetParameterInt("front_matter_data_output_mode") == OUTPUT_FM_TO_SHMS)
+	SendGuiParameterShow("front_matter_data_output_mode", CInt(  hasFrontMatter  ))
+	SendGuiParameterShow("front_matter_prefix", CInt(  hasFrontMatter AND hasFrontMatterPrefix  ))
+	SendGuiParameterShow("front_matter_shm_var_name", CInt(  hasFrontMatter AND GetParameterInt("front_matter_data_output_mode") == OUTPUT_FM_TO_SHM  ))
+	SendGuiParameterShow("front_matter_target", CInt(  hasFrontMatter AND GetParameterInt("front_matter_data_output_mode") == OUTPUT_FM_TO_OTHER  ))
 end sub
 
 sub OnExecPerField()
@@ -65,34 +107,122 @@ sub ReadFile()
 		exit sub
 	end if
 	
-	isLoaded = System.LoadTextFile(filepath, result)
+	isLoaded = System.LoadTextFile(filepath, originalFullText)
 	if Not isLoaded then
 		println("Can not load the file: " & filepath)
 		exit sub
 	end if
 	
-	result.split("\n", arr_result)
-	for i=0 to arr_result.ubound
-		s = arr_result[i]
+	TrimOriginalTextLines() ' to prepare arrOriginalLines
+	
+	if GetParameterBool("has_front_matter") then
+		FindFrontMatterLine() ' to prepare frontMatterLineIndex
+		if frontMatterLineIndex >= 0 then
+			arrFrontMatterLines = GetSubLines(arrOriginalLines, 0, frontMatterLineIndex - 1)
+			arrDataLines = GetSubLines(arrOriginalLines, frontMatterLineIndex + 1 + CInt(GetParameterBool("ignore_first_data_line")), -1)
+			frontMatterText.join(arrFrontMatterLines, "\n")
+			dataText.join(arrDataLines, "\n")
+		else
+			frontMatterText = ""
+			dataText = ""
+		end if
+	else
+		frontMatterText = ""
+		if GetParameterBool("ignore_first_data_line") then arrOriginalLines.Erase(0)
+		trimmedFullText.join(arrOriginalLines, "\n")
+		dataText = trimmedFullText
+	end if
+	
+	OutputFrontMatter()
+	OutputData()
+end sub
+
+sub TrimOriginalTextLines() 
+	originalFullText.split("\n", arrOriginalLines)
+	for i=0 to arrOriginalLines.ubound
+		s = arrOriginalLines[i]
 		s.trim()
 		if GetParameterBool("is_removing_empty_lines") AND s == "" then
-			arr_result.erase(i)
+			arrOriginalLines.erase(i)
 			i -= 1
 		end if
 	next
-	result.join(arr_result, "\n")
-	
-	Output(result)
+End sub
+
+sub FindFrontMatterLine() 
+	frontMatterLineIndex = -1
+	for i=0 to arrOriginalLines.ubound
+		if arrOriginalLines[i].Match("^-{3,}$") then
+			' if the line contains ONLY "-" characters, min is three on them
+			frontMatterLineIndex = i
+			exit sub
+		end if
+	next
+End sub
+
+Function GetSubLines(arrLines As Array[String], startIndex As Integer, endIndex As Integer) As Array[String]
+	Dim arrResultLines As Array[String]
+	if endIndex < 0 then endIndex = arrLines.size + endIndex ' where "-1" gives the last index
+	for i=startIndex to endIndex
+		arrResultLines.Push(arrLines[i])
+	Next
+	GetSubLines = arrResultLines
+End Function
+
+Sub PrepareFrontMatterFields()
+	frontMatterFields.Clear()
+	for i=0 to arrFrontMatterLines.ubound
+		Dim theFieldNameSeparatorIndex = arrFrontMatterLines[i].Find(":")
+		if theFieldNameSeparatorIndex > 0 then
+			field.name = arrFrontMatterLines[i].GetSubstring(0, theFieldNameSeparatorIndex)
+			field.value = arrFrontMatterLines[i].GetSubstring(theFieldNameSeparatorIndex + 1, arrFrontMatterLines[i].length)
+			frontMatterFields.Push(field)
+		end if
+	next
+End Sub
+
+sub OutputFrontMatter()
+	Select Case GetParameterInt("front_matter_data_output_mode")
+	Case OUTPUT_FM_TO_THIS
+		if GetParameterInt("data_output_mode") <> OUTPUT_DATA_TO_THIS_GEOM then
+			this.geometry.text = frontMatterText
+		end if
+	Case OUTPUT_FM_TO_OTHER
+		cFrontMatterDataTarget.geometry.text = frontMatterText
+	Case OUTPUT_FM_TO_CONTAINERS
+		PrepareFrontMatterFields()
+		for i=0 to frontMatterFields.ubound
+			Dim cFrontMatterFiledTarget = Scene.FindContainer(frontMatterPrefix & frontMatterFields[i].name)
+			if cFrontMatterFiledTarget <> null then
+				cFrontMatterFiledTarget.Geometry.Text = frontMatterFields[i].value
+			end if
+		next
+	Case OUTPUT_FM_TO_SHM
+		if shmFrontMatterVarName <> "" then
+			System.Map[shmFrontMatterVarName] = frontMatterText
+		end if
+	Case OUTPUT_FM_TO_SHMS
+		PrepareFrontMatterFields()
+		for i=0 to frontMatterFields.ubound
+			if frontMatterFields[i].name <> "" then
+				System.Map[frontMatterPrefix & frontMatterFields[i].name] = frontMatterFields[i].value
+			end if
+		next
+	End Select
 end sub
 
-sub Output(result as String)
-	Select Case GetParameterInt("output_mode")
-	Case OUTPUT_TO_THIS_GEOM
-		this.geometry.text = result
-	Case OUTPUT_TO_OTHER_GEOM
-		cTarget.geometry.text = result
-	Case OUTPUT_TO_SYSTEM_VAR
-		System.Map[varname] = result
+sub OutputData()
+	Select Case GetParameterInt("data_output_mode")
+	Case OUTPUT_DATA_TO_THIS_GEOM
+		if  GetParameterInt("front_matter_data_output_mode") == OUTPUT_FM_TO_THIS then
+			this.geometry.text = frontMatterText & "\n---\n" & dataText
+		else
+			this.geometry.text = dataText
+		end if
+	Case OUTPUT_DATA_TO_OTHER_GEOM
+		cDataTarget.geometry.text = dataText
+	Case OUTPUT_DATA_TO_SYSTEM_VAR
+		System.Map[shmDataVarName] = dataText
 	End Select
 end sub
 
