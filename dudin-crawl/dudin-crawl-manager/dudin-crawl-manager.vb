6f5589cb34c8b9902b6d93d3be4e81bcddfad3d6
dudintv <dudintv@gmail.com>
2023-12-14 13:23:49 +0100

    make crawl manger work with changing input

--- dudin-crawl/dudin-crawl-manager/dudin-crawl-manager.vb
+++ dudin-crawl/dudin-crawl-manager/dudin-crawl-manager.vb
@@ -1,4 +1,4 @@
-RegisterPluginVersion(1,0,0)
+RegisterPluginVersion(1,0,1)
 Dim info As String = "Crawl manager
 Developer: Dmitry Dudin
 http://dudin.tv"
@@ -6,15 +6,19 @@ http://dudin.tv"
 Dim sourceMode, outputMode As Integer
 Dim arrCrawlItems, arrSourteText As Array[String]
 Dim sourceText, delimiter As String
-Dim cOutput As Container
+Dim cInput, cOutput As Container
 
-Dim sourceModes As Array[String]
+Dim sourceModes, outputModes As Array[String]
 sourceModes.Push("text")
 sourceModes.Push("container")
 sourceModes.Push("SHM")
+outputModes.Push("container")
+outputModes.Push("SHM")
 Dim SOURCE_MODE_TEXT = 0
-Dim SOURCE_MODE_CONATINER = 1
+Dim SOURCE_MODE_CONTAINER = 1
 Dim SOURCE_MODE_SHM = 2
+Dim OUTPUT_MODE_CONTAINER = 0
+Dim OUTPUT_MODE_SHM = 1
 
 sub OnInitParameters()
 	RegisterParameterString("delimiter", "Source delimiter", "\\n", 100, 999, "")
@@ -23,7 +27,7 @@ sub OnInitParameters()
 	RegisterParameterContainer("source_container", " └ Source text container")
 	RegisterParameterString("source_shm", " └ Source SHM variable", "", 100, 999, "")
 
-	RegisterRadioButton("output_mode", "Output mode", 0, sourceModes)
+	RegisterRadioButton("output_mode", "Output mode", 0, outputModes)
 	RegisterParameterContainer("output_container", " └ Output text container (or this)")
 	RegisterParameterString("output_shm", " └ Output SHM variable", "", 100, 999, "")
 
@@ -38,17 +42,29 @@ sub OnInit()
 	outputMode = GetParameterInt("output_mode")
 	delimiter = GetParameterString("delimiter")
 	if delimiter == "\\n" then delimiter = "\n"
+
+	if sourceMode == SOURCE_MODE_CONTAINER then
+		System.Map.UnregisterChangedCallback(GetParameterString("source_shm"))
+
+		cInput = GetParameterContainer("source_container")
+		if cInput <> null then cInput.geometry.RegisterTextChangedCallback()
+	elseif sourceMode == SOURCE_MODE_SHM then
+		if cInput <> null then cInput.geometry.UnregisterChangedCallback()
+
+		System.Map.RegisterChangedCallback(GetParameterString("source_shm"))
+	end if
+
 	cOutput = GetParameterContainer("output_container")
 	if cOutput == null then cOutput = this
 end sub
 sub OnParameterChanged(parameterName As String)
 	OnInit()
 	SendGuiParameterShow("source_text", CInt(sourceMode == SOURCE_MODE_TEXT))
-	SendGuiParameterShow("source_container", CInt(sourceMode == SOURCE_MODE_CONATINER))
+	SendGuiParameterShow("source_container", CInt(sourceMode == SOURCE_MODE_CONTAINER))
 	SendGuiParameterShow("source_shm", CInt(sourceMode == SOURCE_MODE_SHM))
 
-	SendGuiParameterShow("output_container", CInt(outputMode == SOURCE_MODE_CONATINER))
-	SendGuiParameterShow("output_shm", CInt(outputMode == SOURCE_MODE_SHM))
+	SendGuiParameterShow("output_container", CInt(outputMode == OUTPUT_MODE_CONTAINER))
+	SendGuiParameterShow("output_shm", CInt(outputMode == OUTPUT_MODE_SHM))
 	Process()
 end sub
 
@@ -58,11 +74,20 @@ sub OnExecAction(buttonId As Integer)
 	end if
 end sub
 
+sub OnGeometryChanged(geom As Geometry)
+	Process()
+end sub
+
+sub OnSharedMemoryVariableChanged(map As SharedMemory, mapKey As String)
+	Process()
+end sub
+
 Sub Process()
+
 	if sourceMode == SOURCE_MODE_TEXT then
 		sourceText = GetParameterString("source_text")
-	elseif sourceMode == SOURCE_MODE_CONATINER then
-		sourceText = GetParameterContainer("source_container").geometry.text
+	elseif sourceMode == SOURCE_MODE_CONTAINER then
+		sourceText = cInput.geometry.text
 	elseif sourceMode == SOURCE_MODE_SHM then
 		sourceText = CStr(System.Map[GetParameterString("source_shm")])
 	end if
@@ -87,9 +112,10 @@ Sub Process()
 	Dim result = ""
 	result.join(arrCrawlItems, "\n")
 
-	if outputMode == SOURCE_MODE_CONATINER then
+	if outputMode == OUTPUT_MODE_CONTAINER then
 		cOutput.geometry.text = result
-	elseif outputMode == SOURCE_MODE_SHM then
+	elseif outputMode == OUTPUT_MODE_SHM then
 		System.Map[GetParameterString("output_shm")] = result
 	end if
 End Sub
+
