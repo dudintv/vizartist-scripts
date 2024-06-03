20f322ceddb025bd2880c201529925d05d429491
Dmitriy <svetoz@gmail.com>
2024-06-03 23:22:21 +0200

    Update dudin-crawl-engine.vb — add supporting containers and gaps

--- dudin-crawl/dudin-crawl-engine/dudin-crawl-engine.vb
+++ dudin-crawl/dudin-crawl-engine/dudin-crawl-engine.vb
@@ -1,5 +1,5 @@
-RegisterPluginVersion(1,1,5)
-Dim info As String = "Crawl
+RegisterPluginVersion(1,2,0)
+Dim info As String = "Crawl Engine
 Developer: Dmitry Dudin
 http://dudin.tv"
 
@@ -10,11 +10,13 @@ Dim startX, endX As Double
 Dim v1, v2 As Vertex
 
 Dim sourceText As String
-Dim arrSourteText As Array[String]
+Dim arrSourceLines As Array[String]
 Structure CrawlItem
 	sourceText As String
 	type As String
+	parameter As String
 	text As String
+	gap As Double
 	cTemplate As Container
 End Structure
 Dim arrCrawlItems As Array[CrawlItem]
@@ -41,7 +43,7 @@ sub OnInitParameters()
 	RegisterParameterContainer("production", "Production")
 	RegisterParameterContainer("area", "Bounding box area")
 	RegisterParameterDouble("speed", "Speed", 100, 0, 9999)
-	RegisterParameterDouble("gap", "Gap", 10, 0, 9999)
+	RegisterParameterDouble("gap", "Default gap", 10, 0, 9999)
 	RegisterParameterString("delimiter", "Source delimiter", "\\n", 100, 999, "")
 	RegisterRadioButton("source_mode", "Source mode", 0, sourceModes)
 	RegisterParameterText("source_text", "Source text", 100, 100)
@@ -88,9 +90,9 @@ sub OnInit()
 	startX = v2.x
 	endX = v1.x
 
-	Dim arrEventNames As Array[String]
-	arrEventNames.Push("NEW ITEM")
-	Eventpool.registerEvents("Crawl", arrEventNames)
+'	Dim arrEventNames As Array[String]
+'	arrEventNames.Push("NEW ITEM")
+'	Eventpool.registerEvents("Crawl", arrEventNames)
 end sub
 
 sub OnExecAction(buttonId As Integer)
@@ -136,6 +138,9 @@ Sub CleanupProduction()
 	cProduction.position.x = 0
 End Sub
 
+Dim renderableTypes As StringMap
+renderableTypes["text"] = true
+renderableTypes["container"] = true
 Sub PrepareItems()
 	if sourceMode == SOURCE_MODE_TEXT then
 		sourceText = GetParameterString("source_text")
@@ -145,31 +150,59 @@ Sub PrepareItems()
 		sourceText = CStr(System.Map[GetParameterString("source_shm")])
 	end if
 	sourceText.Trim()
-	sourceText.Split(delimiter, arrSourteText)
-	println("sourceText = " & sourceText)
+	sourceText.Split(delimiter, arrSourceLines)
 
 	arrCrawlItems.Clear()
 	if sourceText == "" then exit sub
 
-	for i=0 to arrSourteText.ubound
+	Dim cTextTemplate = cTemplates.FirstChildContainer
+	Dim gap = GetParameterDouble("gap")
+	for i=0 to arrSourceLines.ubound
 		Dim item As CrawlItem
-		item.sourceText = arrSourteText[i]
+		item.sourceText = arrSourceLines[i]
 
 		item.text = item.sourceText
 		item.text.trim()
+		item.gap = gap
+		Dim sItemTextToCompare = item.text
+		sItemTextToCompare.MakeLower()
 		Dim containerTagName = "container"
-		if item.text.StartsWith("<" & containerTagName) then
+		Dim setterTagName = "set"
+		if sItemTextToCompare.StartsWith("<" & containerTagName) then
 			item.type = "container"
 			Dim openTagIndex = 2 + containerTagName.length
 			Dim closeTagIndex = item.text.Find(">")
 			Dim sElementName = item.text.GetSubstring(openTagIndex, closeTagIndex - openTagIndex)
-			item.cTemplate = scene.findContainer(sElementName)
+			sElementName.Trim()
+			item.cTemplate = cTemplates.findSubContainer(sElementName)
+		elseif sItemTextToCompare.StartsWith("<" & setterTagName) Then
+			item.type = "set"
+			Dim openTagIndex = 2 + setterTagName.length
+			Dim closeTagIndex = item.text.Find(">")
+			Dim sSetCommand = item.text.GetSubstring(openTagIndex, closeTagIndex - openTagIndex)
+			Dim iCommandSeparatorIndex = sSetCommand.Find(" ") ' find first space as the separator
+			Dim sParameterName = sSetCommand.Left(iCommandSeparatorIndex)
+			Dim sParameterValue = sSetCommand.Right(sSetCommand.length - iCommandSeparatorIndex - 1)
+			
+			item.parameter = sParameterName
+			if sParameterName == "template" then
+				Dim cTemplate = cTemplates.findSubContainer(sParameterValue)
+				if cTemplate <> null then
+					item.cTemplate = cTemplate
+					cTextTemplate = cTemplate
+				end if
+			elseif sParameterName == "gap" OR sParameterName == "step" then
+				gap = CDbl(sParameterValue)
+			end if
 		elseif item.text <> "" Then
 			item.type = "text"
-			item.cTemplate = cTemplates.FirstChildContainer
+			item.cTemplate = cTextTemplate
 		end if
-		arrCrawlItems.Push(item)
-		'PrintItem(item)
+		
+		if renderableTypes.ContainsKey(item.type) then
+			arrCrawlItems.Push(item)
+		end if
+		PrintItem(item)
 	next
 End Sub
 
@@ -189,18 +222,20 @@ Sub InsertNextItem()
 	Dim cLast = cProduction.FirstChildContainer
 	Dim newItemPositionX = startX
 	if cLast <> null then
-		newItemPositionX = cLast.position.x + cLast.GetTransformedBoundingBoxDimensions().x + GetParameterDouble("gap")
+		newItemPositionX = cLast.position.x + cLast.GetTransformedBoundingBoxDimensions().x + arrCrawlItems[currentItemIndex].gap
 	end if
-	if newItemPositionX < startX then newItemPositionX = startX ' to avoid instant showing the new element in the middle of the crawl
+	'if newItemPositionX < startX then newItemPositionX = startX ' to avoid instant showing the new element in the middle of the crawl
 	
 	if currentItemIndex > arrCrawlItems.ubound then exit sub
 
 	Dim cNew As Container = arrCrawlItems[currentItemIndex].cTemplate.CopyTo(cProduction, TL_DOWN)
 	cNew.position.xyz = CVertex(newItemPositionX, 0, 0)
-	if arrCrawlItems[currentItemIndex].type == "container" Then
-		'nothing to change
-	else
-		cNew.geometry.text = arrCrawlItems[currentItemIndex].text
+	if arrCrawlItems[currentItemIndex].type == "text" Then
+		if cNew.geometry.text <> null then
+			cNew.geometry.text = arrCrawlItems[currentItemIndex].text
+		else
+			cNew.findSubContainer("=text").geometry.text = arrCrawlItems[currentItemIndex].text
+		end if
 	end if
 	'cNew.RecomputeMatrix()
 	arrActiveItems.push(cNew)
@@ -230,13 +265,14 @@ End Sub
 
 Sub DetectAndCreateNextItem()
 	cProduction.GetTransformedBoundingBox(v1, v2)
-	if v2.x < startX - GetParameterDouble("gap") then
+	Dim currentGap = arrCrawlItems[currentItemIndex].gap
+	if startX + currentGap > v2.x then
 		InsertNextItem()
 	end if
 End Sub
 
 Sub PrintItem(item As CrawlItem)
-	println("type=" & item.type & "| text=" & item.text & "|c=" & item.cTemplate.name)
+	println("type=" & item.type & "| text=" & item.text & "|c=" & item.cTemplate.name & "|p=" & item.parameter & "|gap=" & item.gap)
 End Sub
 
 Sub Report()
