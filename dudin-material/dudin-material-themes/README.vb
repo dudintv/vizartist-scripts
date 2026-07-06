30bf73fdd5ad8f9dc0c5f4ef998c2880411a14f6
Dmitriy <dudin.tv@gmail.com>
2026-07-06 19:31:23 +0200

    Create README.vb for Material Theme Switcher
    Add README for Material Theme Switcher with version history.

---
+++ dudin-material/dudin-material-themes/README.vb
@@ -0,0 +1,11 @@
+# Material Theme Swither
+
+### 1.1.0
+
+* the Current Theme can be changed by Omo plugin on the Source Root container
+
+### 1.0.0
+
+* support multiple themes with multiple containers with materials that represent "tags"
+* support format tags: "<tag>" where "tag" is the tag name
+* possible to change the theme by number "Current Theme" in UI
