99589d6d9d867d27fbe48fb2228cd4e5b7f8fc09
Dmitriy <dudin.tv@gmail.com>
2026-07-08 19:55:30 +0200

    Revise version history in README.vb
    Updated version numbers and release dates in README.

--- dudin-material/dudin-material-themes/README.vb
+++ dudin-material/dudin-material-themes/README.vb
@@ -1,14 +1,14 @@
 # Material Theme Swither
 
-### 1.2.0
+### 1.2.0 — 8 JUne 2026
 
 * add support Omo switching by tag suffix, for example: "<bg:omo>"
 
-### 1.1.0
+### 1.1.0 — 7 JUne 2026
 
 * the Current Theme can be changed by Omo plugin on the Source Root container
 
-### 1.0.0
+### 1.0.0 — 6 JUne 2026
 
 * support multiple themes with multiple containers with materials that represent "tags"
 * support format tags: "<tag>" where "tag" is the tag name
