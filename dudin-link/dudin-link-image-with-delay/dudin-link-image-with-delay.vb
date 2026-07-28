acbcd3a186787ea6c4aba4a3b03a1cfea37ae70a
Dmitriy <dudin.tv@gmail.com>
2026-07-28 16:04:18 +0200

    Update plugin version and sync modes

--- dudin-link/dudin-link-image-with-delay/dudin-link-image-with-delay.vb
+++ dudin-link/dudin-link-image-with-delay/dudin-link-image-with-delay.vb
@@ -1,12 +1,12 @@
-RegisterPluginVersion(1,0,0)
+RegisterPluginVersion(1,1,0)
 
 '''''''''''''''''''''''
 ' Change it as you need:
-Dim TARGETS_AMOUNT = 3
+dim TARGETS_AMOUNT = 1
 '''''''''''''''''''''''
 
-Dim cSource As Container
-Dim arrcTargets As Array[Container]
+dim cSource As Container
+dim arrcTargets As Array[Container]
 
 dim tick = 0
 dim hasImageChanged = false
@@ -14,9 +14,21 @@ dim previousImage As Image
 dim imgName = "None"
 dim prevName = "None"
 
+dim buttonNames As Array[String]
+buttonNames.Push("Immediate")
+buttonNames.Push("Delay")
+buttonNames.Push("By button")
+dim MODE_IMMEDIATE = 0
+dim MODE_DELAY = 1
+dim MODE_BUTTON = 2
+dim currentMode As Integer
+
 sub OnInitParameters()
 	RegisterParameterContainer("source", "Source (or this)")
+	RegisterRadioButton("mode", "Sync mode", MODE_DELAY, buttonNames)
 	RegisterParameterInt("delay", "Sync delay (in frames)", 0, 0, 999999)
+	RegisterPushButton("sync_now", "Sync now", 1)
+	RegisterParameterBool("sync_init", "Sync immediate when init", true)
 	for i=1 to TARGETS_AMOUNT
 		RegisterParameterContainer("target" & i, "Target " & i)
 	next
@@ -34,6 +46,18 @@ sub OnInit()
 			arrcTargets.Push(c)
 		end if
 	next
+	
+	if GetParameterBool("sync_init") then
+		SyncImage()
+	end if
+end sub
+
+sub OnParameterChanged(parameterName As String)
+	if parameterName == "sync_now" then exit sub
+	OnInit()
+	SendGuiParameterShow("delay", CInt(GetParameterInt("mode") == MODE_DELAY))
+	SendGuiParameterShow("sync_now", CInt(GetParameterInt("mode") == MODE_BUTTON))
+	SendGuiParameterShow("sync_init", CInt(GetParameterInt("mode") <> MODE_IMMEDIATE))
 end sub
 
 sub OnExecPerField()
@@ -42,22 +66,30 @@ sub OnExecPerField()
 	end if
 		
 	hasImageChanged = cSource.Texture.Image <> previousImage 
-	
 
-	if cSource.Texture.Image <> null then imgName = cSource.Texture.Image.Name
-	if previousImage <> null then prevName = previousImage.Name
-	
 	if hasImageChanged then
-		tick += 1
+		currentMode = GetParameterInt("mode")
 		
-		if tick >= GetParameterInt("delay") then
-			tick = 0
+		if currentMode == MODE_IMMEDIATE then
+			' Mode 0: Sync right away
 			SyncImage()
+			
+		elseif currentMode == MODE_DELAY then
+			' Mode 1: Wait for X frames before syncing
+			tick += 1
+			if tick >= GetParameterInt("delay") then
+				SyncImage()
+			end if
+			
+		elseif currentMode == MODE_BUTTON then
+			' Mode 2: Do nothing. Wait for the user to press the button.
 		end if
+	else
+		' If the image hasn't changed (or was already synced), reset the tick counter
+		tick = 0
 	end if
 end sub
 
-SyncImage()
 sub SyncImage()
 	if cSource == null OR cSource.Texture == null then
 		exit sub
@@ -69,3 +101,11 @@ sub SyncImage()
 		arrcTargets[i].Texture = cSource.Texture
 	next
 end sub
+
+
+sub OnExecAction(buttonId As Integer)
+	if buttonId == 1 then
+		SyncImage()
+	end if
+end sub
+
